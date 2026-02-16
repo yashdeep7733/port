062493073c1650ba142b0e211e912b8a46b8d4b8
Deepyash <158832695+yashLGN@users.noreply.github.com>
2026-02-16 17:04:38 +0530

    linkdin addded

--- frontend/src/github_buttons.jsx
+++ frontend/src/github_buttons.jsx
@@ -4,7 +4,7 @@ export default function Github_buttons() {
   return (
     <div className="github-buttons">
     <a type="button" href='https://github.com/yashLGN' className="btn btn-lg me-2 github-button">Github</a>
-    <a type="button" href='https://www.youtube.com/@Lgn7733' className="btn btn-outline-danger btn-lg ms-2">Music Channel</a>
+    <a type="button" href='https://www.linkedin.com/in/yashdeep7733/' className="btn btn-outline-danger btn-lg ms-2">LinkedIn</a>
     </div>
   )
 }
