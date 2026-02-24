dcd188b73aba3e4b8110fb569237d315f00e5b75
Deepyash <158832695+yashLGN@users.noreply.github.com>
2026-02-24 07:51:23 +0530

    terminal values changed

--- frontend/src/terminal.jsx
+++ frontend/src/terminal.jsx
@@ -9,13 +9,13 @@ export default function Terminal() {
 
       <pre className="terminal-code m-0 px-3 py-2">
 <span className="line"><span className="text-white">1</span> {'{'} </span>
-<span className="line"><span className="text-white">2</span>   <span className="text-info">"web app"</span>: <span className="text-danger">"about yash"</span>,</span>
-<span className="line"><span className="text-white">3</span>   <span className="text-info">"version"</span>: <span className="text-warning">beta 1.0</span>,</span>
-<span className="line"><span className="text-white">4</span>   <span className="text-info">"open_source"</span>: <span className="text-success">no idea</span>,</span>
+<span className="line"><span className="text-white">2</span>   <span className="text-info">"web app"</span>: <span className="text-danger">"about yashdeep"</span>,</span>
+<span className="line"><span className="text-white">3</span>   <span className="text-info">"version"</span>: <span className="text-warning">beta 1.6</span>,</span>
+<span className="line"><span className="text-white">4</span>   <span className="text-info">"open_source"</span>: <span className="text-success">sureeE</span>,</span>
 <span className="line"><span className="text-white">5</span>   <span className="text-info">"tags"</span>: [</span>
-<span className="line"><span className="text-white">6</span>   <span className="text-danger">"application"</span>,</span>
-<span className="line"><span className="text-white">7</span>   <span className="text-danger">"developer"</span>,</span>
-<span className="line"><span className="text-white">8</span>   <span className="text-danger">"web-design"</span></span>
+<span className="line"><span className="text-white">6</span>   <span className="text-danger">"developer"</span>,</span>
+<span className="line"><span className="text-white">7</span>   <span className="text-danger">"ai development"</span>,</span>
+<span className="line"><span className="text-white">8</span>   <span className="text-danger">"web-development"</span></span>
 <span className="line"><span className="text-white">9</span>   ]</span>
 <span className="line"><span className="text-white">10</span> {'}'} </span>
 <span className="line">
